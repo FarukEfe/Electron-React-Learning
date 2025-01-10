fe10acbe6f65ef9c12d795d0e6e4e39c5f193b15
Efe Yencilek <64659675+FarukEfe@users.noreply.github.com>
2025-01-09 23:10:09 -0500

    Configure for OS and build difference for development and deployment mode

--- src/Electron/main.ts
+++ src/Electron/main.ts
@@ -1,8 +1,14 @@
 import {app, BrowserWindow} from 'electron';
 import path from 'path';
+import { isDev } from './util.js';
 
 app.on('ready', () => {
     const mainWindow = new BrowserWindow({});
     mainWindow.loadFile(path.join(app.getAppPath(), '/dist-react/index.html'));
-    
+    if (isDev()) {
+        // The runtime in development mode
+
+    } else {
+        // The runtime in deployment mode
+    }
 });
